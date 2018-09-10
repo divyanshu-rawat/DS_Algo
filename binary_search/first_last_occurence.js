796962d359cc176decd30ec0f2ed00644ff4f3ec
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-09-10 17:49:12 +0530

    done

--- binary_search/first_last_occurence.js
+++ binary_search/first_last_occurence.js
@@ -3,33 +3,29 @@
 
 
 
-let array = [1,2,2,2,2,3,3,3,3,4,5,6,7,8,9,10];
-let res = null;
+let array = [1,2,3,5,6,6,7,8,9,10];
+let res = -1;
 
-binary_search(0,array.length - 1,array, 3);
+console.log(binary_search(0,array.length - 1,array, 19));
 
 
 function binary_search(start, end , a, x) {
 	
+	while(start <= end){
 
-while(start <= end){
+		let mid = (start + end)/2;
+		mid = Math.floor(mid);
 
-	let mid = (start + end)/2;
-	mid = Math.floor(mid);
-
-	console.log('value of mid', a[mid]);
-
-	if(a[mid] >= x){
-		console.log('--', mid);
-		res = mid;
-		end = mid - 1;
-	}
-	else{
-		console.log('once',mid);
-		start = mid + 1;
+		if(a[mid] == x){
+			return mid;
+		}
+		else if(a[mid] > x){
+			end = mid - 1;
+		}
+		else{
+			start = mid + 1;
+		}
 	}
 
-}
-
-console.log('First Occurence ->',res);
+	return -1;
 }
